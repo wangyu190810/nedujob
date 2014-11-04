7ea1e6e5c84c09c29c975735108954071f903eb0
wangyu <wangyu@itianpin.com>
2014-11-04 10:42:53 +0800

    创建配置文件

---
+++ config.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+#-*- coding:utf-8 -*-
+#Filename: config.py
+#Author: wangyu190810
+#E-mail: wo190810401@gmail.com
+#Date: 2014-11-04
+#Description: 
+
+class Config(object):
+    db = {"host":"localhost",
+            "db":"ndeujob",
+            "user":"postgres",
+            "password":"wangyu190810"
+            }
+
+
