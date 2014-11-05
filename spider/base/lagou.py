57a0ab69d431568c784ace2a98a83fbdf7a670d3
wangyu <wangyu@itianpin.com>
2014-11-05 20:01:08 +0800

    现在能得到拉钩的每个页面的信息，具体下来怎么做，还要看我的设计和负载问题考虑

--- spider/base/lagou.py
+++ spider/base/lagou.py
@@ -11,17 +11,24 @@
 from  base import Base
 
 class Lagou(Base):
+    def __init__(self):
+        return 
     site = "http://www.lagou.com/jobs/list_python?kd=python&spc=&pl=&gj=&xl=&yx=&gx=&st=&labelWords=&lc=&workAddress=&city="
     base_site = Base(site)
-    html =  base_site.request_site()
-    
-    def get_html(self):
-        print self.html
+    html = base_site.request_site()
+    content = base_site.get_content(html,"clearfix",1)    
+     
+    def get_message(self):
+        for message in self.content:
+            company_url = self.base_site.get_content(str(message),"a",0)
+            print company_url
 
 
-if __name__ == "__mian__":
+
+
+        
+
+
+if __name__ == "__main__":
     lagou_site = Lagou()
-    print lagou_site.html
-    lagou_site.get_html()
-    #print lagou_site.html
-    #print site
+    lagou_site.get_message()
