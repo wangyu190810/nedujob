8095fb067a19ea95fc0ec868d3bcb060d2df44c9
wangyu <wangyu@itianpin.com>
2014-11-05 16:06:41 +0800

    爬虫抓取的基本模块

--- spider/base/base.py
+++ spider/base/base.py
@@ -6,5 +6,26 @@
 #Date: 2014-11-04
 #Description: 
 
+import urllib2
+import requests
+
 """"基础类的建设，主要是一些抓取模块的基础类"""
+
+
+#拿到网站的链接，返回html文件，等待对应的模块解析各自的文件
+class Base(object):
+    def __init__(self,site=None):
+        if site is None:
+            raise
+        else:
+            self.site = site
+    def request_site(self):
+        site_html = requests.get(
+                url = self.site
+                )
+        print site_html.encoding 
+        html =  site_html.text
+        return html 
+
+
 
