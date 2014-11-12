ad94336712da36360287963192aa866900431dd5
wangyu <wangyu@itianpin.com>
2014-11-12 22:50:39 +0800

    增加了一些初始化的model

--- spider/model/base.py
+++ spider/model/base.py
@@ -6,4 +6,19 @@
 #Date: 2014-11-05
 #Description: 
 
+from sqlalchemy.shema import metaData
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import as_declarative
+metadata = metaData()
+
+@as_declarative(metadata=metadata)
+class Base(object):
+    pass
+
+
+engine = create_engine("mysql://root:1234@localhost/nedujob?charset=utf8",echo=True)
+conn = engine.connect()
+
+
 """数据库的一些抽象"""
+
