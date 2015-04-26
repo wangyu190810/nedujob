6f41b070ab7323a4a67e2ef9daf8d375702ddb33
wangyu <190810401@qq.com>
2015-04-27 06:33:26 +0800

    配置文件

---
+++ config-sample.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+#-*- coding:utf-8 -*-
+#Filename: config.py
+#Author: wangyu190810
+#E-mail: wo190810401@gmail.com
+#Date: 2014-11-04
+#Description: 
+
+class Config(object):
+    db = "postgresql://postgres:2015@localhost/nedujob"
+    SUCCESS_KEY = u"adsfsdf"
+    salt = u"asdfa"
+    pic = ""
+    def lziy(self):
+        pass
+
